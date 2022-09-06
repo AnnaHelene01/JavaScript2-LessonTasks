2b60c86be774a40ecc2e9a64a2107be80b02e815
AnnaHelene01 <annahelene2001@gmail.com>
2022-09-06 22:44:00 +0200

    Organizing the repo

---
+++ Modul 1/Modul 1.1/Lesson task 1.1/script.js
@@ -0,0 +1,21 @@
+/**
+ * This will add together value of a plus the value of b. 
+ * @param {number} a This is the value of a
+ * @param {number} b This is the value of b
+ * @returns {number} The total value of a plus b is returned
+ * @example
+ * ```js
+ * //Use this function to add together
+ * //value of a plus value of b
+ * const result = addNumbers(4, 6);
+ * //Returns the total value of a and b
+ * //10
+ * ```
+ */
+
+function addNumbers(a, b) {
+    const result = a + b;
+    return result;
+  }
+
+  addNumb
